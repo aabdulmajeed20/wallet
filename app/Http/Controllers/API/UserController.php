f2ea5b11135afc77068a29a0539514ba2dd9ba27
ahmed-abdullah9 <ahmed01799@gmail.com>
2019-03-15 23:18:27 +0300

    api and model

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -39,15 +39,15 @@ class UserController extends Controller
             'password' => 'required', 
             're-password' => 'required|same:password', 
         ]);
-if ($validator->fails()) { 
+        if ($validator->fails()) { 
             return response()->json(['error'=>$validator->errors()], 401);            
         }
-$input = $request->all(); 
+        $input = $request->all(); 
         $input['password'] = bcrypt($input['password']); 
         $user = User::create($input); 
         $success['token'] =  $user->createToken('MyApp')-> accessToken; 
         $success['name'] =  $user->name;
-return response()->json(['success'=>$success], $this-> successStatus); 
+        return response()->json(['success'=>$success], $this-> successStatus); 
     }
 /** 
      * details api 
--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -8,13 +8,14 @@ use App\Ewallet;
 use App\Notifications\InvoicePaid;
 use App\User;
 use Auth;
+use Validator;
 
 class WalletController extends Controller
 {
 
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth:api');
     }
     public function index() {
         $data = Ewallet::all();
@@ -22,4 +23,27 @@ class WalletController extends Controller
         return view('home', ['data' => $data, 'user' => $user]);
     }
 
+    // add the cbx to the user's wallet
+    /** 
+     *
+        * 
+        * @return \Illuminate\Http\Response 
+        */ 
+    public function plusWallet(Request $request)
+    {
+        
+        $validator = Validator::make($request->all(), [ 
+            'user_id' => 'required', 
+            'cbx' => 'required', 
+        ]);
+        if ($validator->fails()) { 
+            return response()->json(['error'=>$validator->errors()], 401);            
+        }
+        $input = $request->all();
+        
+        $wallet = Ewallet::where('user_id', $input['user_id'])->first();
+        $wallet->balance += $input['cbx'];
+        $wallet->save();
+        return response()->json("success", 200);
+    }
 }
